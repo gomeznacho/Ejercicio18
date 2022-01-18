8c2bad3c2525656c32b0c483a728bf420d5c651e
J.Jonez <jimbo.greender@gmail.com>
2022-01-18 10:45:10 +0100

    second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+      /* ejercicio 18
+
         Excepciones.dividePorCero(5);
         Excepciones.buscarArchivo("talycual.tal");
 
         String[] array = {"uno", "dos", "tres", "cuatro", "cinco", "seis"};
         Excepciones.buscarNoExistente(array, 9);
+    */
+
+        //ejercicio 21
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
